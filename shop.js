16ce7726dcf0a85e447680bf83506ef63e5dd8f9
HaydnTakesAminor <haydntakesaminor@gmail.com>
2026-02-27 12:53:05 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,1 +1,20 @@
+const overlay = document.getElementById("itemOverlay");
+const modalImage = document.getElementById("modalImage");
+const closeBtn = document.getElementById("closeModal");
 
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", () => {
+    modalImage.src = img.src;
+    overlay.classList.add("active");
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", (e) => {
+  if (e.target === overlay) {
+    overlay.classList.remove("active");
+  }
+});
