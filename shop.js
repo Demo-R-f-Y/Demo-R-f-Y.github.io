41f1fb94754c3fd475a8f6662c81faceeeba97ea
HaydnTakesAminor <haydntakesaminor@gmail.com>
2026-02-28 11:57:27 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,5 +1,3 @@
-let scrollPosition = 0;
-
 const overlay = document.getElementById("itemOverlay");
 const modalImage = document.getElementById("modalImage");
 const closeBtn = document.getElementById("closeModal");
@@ -7,37 +5,26 @@ const closeBtn = document.getElementById("closeModal");
 // Открытие item
 document.querySelectorAll(".gallery img").forEach(img => {
   img.addEventListener("click", () => {
-    // запоминаем позицию скролла
-    scrollPosition = window.scrollY;
-
-    // подставляем картинку
     modalImage.src = img.src;
-
-    // показываем overlay
     overlay.classList.add("active");
 
-    // фиксируем страницу
+    document.documentElement.classList.add("no-scroll");
     document.body.classList.add("no-scroll");
-    document.body.style.top = `-${scrollPosition}px`;
   });
 });
 
-// Закрытие через крестик
+// Закрытие
 closeBtn.addEventListener("click", closeOverlay);
 
-// Закрытие по клику на фон
 overlay.addEventListener("click", (e) => {
   if (e.target === overlay) {
     closeOverlay();
   }
 });
 
-// Функция закрытия
 function closeOverlay() {
   overlay.classList.remove("active");
 
+  document.documentElement.classList.remove("no-scroll");
   document.body.classList.remove("no-scroll");
-  document.body.style.top = "";
-
-  window.scrollTo(0, scrollPosition);
 }
